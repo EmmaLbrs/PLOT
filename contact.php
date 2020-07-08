3adc939ea43ff076968302d1a3131c48462d3120
emmalbrs <emma.lesburgueres@gmail.com>
2020-07-08 17:12:58 +0200

    ajout lien a propos fil d'ariane

--- contact.php
+++ contact.php
@@ -121,7 +121,7 @@
     <div class="container-fluid">
 
         <div class="fil_ariane">
-            <p><a href="index.html">PLOT</a> > <a href="#">A propos</a> > <a href="contact.php">Nous contacter</a></p>
+            <p><a href="index.html">PLOT</a> > <a href="apropos.html">A propos</a> > <a href="contact.php">Nous contacter</a></p>
         </div>
 
         <div class="row">
